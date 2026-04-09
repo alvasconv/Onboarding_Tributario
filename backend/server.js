7e70b97da7ab3f97330bad3fbe6148b4a6fa2ade
Angello Vasconez <alvasconv@outlook.com>
2026-04-09 00:55:48 -0500

    (feature): Conexión establecida con N8N

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const { validarDatos, validarArchivoPDF } = require('./middleware');
 const multer = require('multer'); // La nueva librería para manejar el PDF
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs'); // Librería nativa de Node para leer archivos del disco
 
 const app = express();
 const PORT = process.env.PORT;
@@ -21,7 +24,7 @@ const upload = multer({ dest: 'uploads/' });
 // OJO AL ORDEN: 
 // 1ro Multer agarra el archivo ('documento_pdf') -> 2do Zod valida todo -> 3ro Ejecutamos
 // ==========================================
-app.post('/api/onboarding', upload.single('documento_pdf'), validarDatos, validarArchivoPDF, (req, res) => {
+app.post('/api/onboarding', upload.single('documento_pdf'), validarDatos, validarArchivoPDF, async (req, res) => {
     
     const datosCliente = req.body;
     const archivo = req.file; // Multer nos deja toda la info del archivo aquí
@@ -33,6 +36,34 @@ app.post('/api/onboarding', upload.single('documento_pdf'), validarDatos, valida
     console.log("=======================================");
 
     // Aquí (en el futuro) conectaremos el código que le envía esto a n8n
+    try {
+        console.log("⚙️ Enviando datos a n8n...");
+        const form = new FormData();
+        
+        // 1. Enviamos los datos de texto (que n8n leerá en $json.body)
+        form.append('ruc', datosCliente.ruc);
+        form.append('razon_social', datosCliente.razon_social);
+        
+        // 2. Enviamos el PDF. ¡OJO! El nombre 'data' coincide exactamente con tu Nodo 2
+        form.append('data', fs.createReadStream(archivo.path));
+
+        // 3. Disparamos el Webhook (Reemplaza la URL por la tuya de Test de n8n)
+        const URL_WEBHOOK_N8N = process.env.URL_N8N_WEBHOOK;
+        
+        await axios.post(URL_WEBHOOK_N8N, form, {
+            headers: form.getHeaders() // Headers obligatorios para enviar archivos
+        });
+
+        console.log("🚀 ¡Webhook disparado con éxito!");
+
+        // 4. (Opcional) Borramos el archivo temporal de nuestro servidor para ahorrar espacio
+        fs.unlinkSync(archivo.path);
+
+    } catch (error) {
+        console.error("❌ Error al contactar con n8n:", error.message);
+        return res.status(500).json({ error: "Fallo la automatización interna" });
+    }
+
 
     res.status(200).json({
         mensaje: "Cliente procesado con éxito",
